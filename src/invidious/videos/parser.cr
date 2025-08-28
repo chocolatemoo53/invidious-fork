36ac67610d1e52df731a21525fac966413541fa9
Fijxu <fijxu@nadeko.net>
2025-08-27 20:23:39 -0400

    fix(parser): fix unavailable comments on videos

--- src/invidious/videos/parser.cr
+++ src/invidious/videos/parser.cr
@@ -410,9 +410,9 @@ def parse_video_info(video_id : String, player_response : Hash(String, JSON::Any
   # Comments enabled?
   comments_enabled = false
 
-  # When comments are enabled there should be a comments-entry-point section in the primary results
+  # When comments are enabled there should be a comment-item-section section in the primary results
   if primary_results
-    section = primary_results.as_a.find(&.dig?("itemSectionRenderer", "sectionIdentifier").== "comments-entry-point")
+    section = primary_results.as_a.find(&.dig?("itemSectionRenderer", "targetId").== "comments-section")
 
     if section
       comments_enabled = true
