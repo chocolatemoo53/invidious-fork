65d7bad723c51995e0cff2b4c81feed3ac9a4395
Fijxu <fijxu@nadeko.net>
2025-09-12 23:40:49 -0300

    remove automatic config reload
    Not really needed anymore

--- src/invidious.cr
+++ src/invidious.cr
@@ -31,7 +31,6 @@ require "yaml"
 require "compress/zip"
 require "protodec/utils"
 require "redis"
-require "inotify"
 
 require "./invidious/database/*"
 require "./invidious/database/migrations/*"
@@ -60,19 +59,6 @@ end
 alias IV = Invidious
 
 CONFIG = Config.load
-
-Signal::HUP.trap do
-  Config.reload
-end
-
-{% if flag?(:linux) %}
-  if CONFIG.reload_config_automatically
-    Inotify.watch("config/config.yml") do |event|
-      Config.reload
-    end
-  end
-{% end %}
-
 HMAC_KEY = CONFIG.hmac_key
 
 PG_DB = begin
--- src/invidious/config.cr
+++ src/invidious/config.cr
@@ -254,10 +254,6 @@ class Config
 
   property backend_name_prefix : String = "Backend"
 
-  {% if flag?(:linux) %}
-    property reload_config_automatically : Bool = true
-  {% end %}
-
   def disabled?(option)
     case disabled = CONFIG.disable_proxy
     when Bool
